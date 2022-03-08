7e1af4b6f0b93eb408e624a0013f0594ec734d91
ehak123 <73587185+ehak123@users.noreply.github.com>
2022-03-08 14:34:40 +0100

    admin ongoing event page 1 - working progress bar

--- components/adminongoingview.js
+++ components/adminongoingview.js
@@ -1,35 +1,26 @@
 import admin from '../styles/adminongoingview.module.css'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
 
 
 export const siteTitle = 'STH Speed Dating'
 
 export function PageHeader() {
   return (
-      <div className={utilStyles.container}>
-        <header className={startpage.header}>
           <>
-          <div className={utilStyles.container}>
-            <section id={startpage.container}>
             <div className={admin.container}>
               <ul className={admin.progressbar}>
-                <li className={active}> Matchning 1 </li>  
-                <li> Presentera M1 </li>  
+                <li className={admin.active}>ç Matchning 1 </li>  
+                <li> Presentera matchning 1 </li>  
                 <li> Date pågår </li>
                 <li> Matchning 2 </li>  
-                <li> Presentera M2 </li>  
+                <li> Presentera matchning 2 </li>  
                 <li> Date pågår </li>
                 <li> Matchning 3 </li>  
-                <li> Presentera M3 </li>  
+                <li> Presentera matchning 3 </li>  
                 <li> Date pågår </li>  
                 <li> Kvällens resultat! </li>  
               </ul >
             </div>
-            </section>
-          </div>
           </>
-        </header>
-      </div>
   )
 }
